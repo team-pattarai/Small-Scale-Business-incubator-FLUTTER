e4a3564b36c882104116ace8d116089380234e4a
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-05-08 22:09:56 +0530

    added homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_application_1/components/DrawerItems.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => GeeksforGeeksState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GeeksforGeeksState extends State<MainScreen> {
+  int currentIndex = 0;
+  List<Color> containerGradient = [Color.fromARGB(255, 253, 255, 253), Color(0xFF76ABF3)];
 
-  // This widget is the root of your application.
+  final List<List<Color>> colors = [
+    [Color.fromARGB(255, 246, 255, 246), Color(0xFF76ABF3)],
+    [Color(0xFFA1C4FD), Color(0xFFC2E9FB)],
+    [Color(0xFF8CF385), Color(0xFF76ABF3)],
+    [Color(0xFFA1C4FD), Color(0xFFC2E9FB)],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(193, 233, 233, 1),
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25))),
+          elevation: .3,
+          backgroundColor: Color.fromRGBO(181, 243, 243, 1),
+          title: const Text("Project name :)"),
+        ),
+        drawer: Drawer(
+          backgroundColor: Color.fromRGBO(196, 223, 223, 1),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(child: const Text("Project Name")),
+              const DropDownText(
+                  label: "Business", icon: Icons.workspaces_filled, buttonRack: []),
+            ],
+          ),
+        ),
+        body: Container(
+          height: 475,
+          width: double.infinity,
+          margin: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(217, 238, 238, 1),
+              borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.all(15),
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
+            children: List.generate(
+              12,
+              (index) => Container(
+                padding: EdgeInsets.all(10),
+                height: 75,
+                width: 75,
+                color: Color.fromRGBO(248, 246, 244, 1),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: _Navbar(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget _Navbar() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(147, 228, 228, 1),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 19, 76, 78).withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: GNav(
+          gap: 8,
+          duration: const Duration(milliseconds: 500),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+              backgroundGradient: const LinearGradient(
+                colors: [Color.fromRGBO(181, 243, 243, 1), Color.fromRGBO(248, 246, 244,1)],
+              ),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 0;
+                  containerGradient = colors[0];
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            GButton(
+              icon: Icons.pie_chart,
+              text: 'Stats',
+              backgroundGradient: const LinearGradient(
+                colors: [Color.fromRGBO(181, 243, 243, 1), Color.fromRGBO(248, 246, 244,1)],
+              ),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 1;
+                  containerGradient = colors[1];
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.manage_history,
+              text: 'Orders',
+              backgroundGradient: const LinearGradient(
+                  colors: [Color.fromRGBO(181, 243, 243, 1), Color.fromRGBO(248, 246, 244,1)],),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 2;
+                  containerGradient = colors[2];
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.person,
+              text: 'Profile',
+              backgroundGradient: const LinearGradient(
+                colors: [Color.fromRGBO(181, 243, 243, 1), Color.fromRGBO(248, 246, 244,1)],
+              ),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 3;
+                  containerGradient = colors[3];
+                });
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void main() {
+  runApp(const MainScreen());
 }
